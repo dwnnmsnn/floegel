649096d4262960fc5eb80565365cd0391db62b86
dwnnmsnn <152543815+dwnnmsnn@users.noreply.github.com>
2024-01-01 10:11:58 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function showSidemenu () {
+document.getElementById('sidemenu').style.display = 'block';
+document.getElementById('home').style.display = 'none';
+document.getElementById('prd').style.display = 'none';
+document.getElementById('impr').style.display = 'none'
+}
+
+function closeSidemenu () {
+document.getElementById('sidemenu').style.display = 'none';
+document.getElementById('home').style.display = 'block';
+document.getElementById('prd').style.display = 'block';
+document.getElementById('impr').style.display = 'block'
+}
